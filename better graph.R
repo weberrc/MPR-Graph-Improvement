4f1ed60aee68a79dc28d75177f580da8700ac104
rachel.weber@state.co.us <85964287+weberrc@users.noreply.github.com>
2022-03-24 17:07:06 -0500

    changed font sizes

--- better graph.R
+++ better graph.R
@@ -18,18 +18,19 @@ dat$name <- factor(dat$name, levels = c( "Deaths", "Births", "International.Migr
 ggplot(dat, aes(x = Year, y = value, fill = name)) +
   geom_col() +
   geom_line(aes(y = Net.Change), size = 1.2) +
-  geom_text(x = 2020.5, y = 25000, label = "Net Population\nChange", size = 4.5) +
+  geom_text(x = 2020, y = 31000, label = "Net Population\nChange", size = 5) +
   coord_cartesian(clip = "off") +
-  ylab("Population Change") +
+  ylab("Population\nChange") +
+  xlab("") +
   theme_minimal() +
   theme(legend.position = "top",
-        axis.text.x = element_text(size = 12),
-        axis.text.y = element_text(size = 12),
-        legend.text = element_text(size = 10),
-        plot.title = element_text(size = 18),
-        legend.title = element_text(size = 12),
-        axis.title.y = element_text(size = 14, angle = 0),
-        axis.title.x = element_text(size = 14)) +
+        axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        legend.text = element_text(size = 12),
+        plot.title = element_text(size = 20),
+        plot.subtitle = element_text(size = 14),
+        axis.title.y = element_text(size = 16, angle = 0),
+        axis.title.x = element_text(size = 16)) +
   scale_y_continuous(breaks = c(-50000, -25000, 0, 25000, 50000, 75000)) +
   labs(fill = "") +
   scale_fill_manual(values = c("#527d03", "#A67F8E", "#0a5189", "#f38119"),
